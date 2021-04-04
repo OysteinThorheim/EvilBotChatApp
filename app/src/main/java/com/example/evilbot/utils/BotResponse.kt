73efebb5abe4daa89f238baf0ab739892bef45e3
joeych8 <73697534+joeych8@users.noreply.github.com>
2021-04-04 19:34:30 +0200

    shared

--- app/src/main/java/com/example/evilbot/utils/BotResponse.kt
+++ app/src/main/java/com/example/evilbot/utils/BotResponse.kt
@@ -8,25 +8,59 @@ object BotResponse {
         val message = _message.toLowerCase()
 
         return when {
-            //Hello
-            message.contains("hello") -> {
+
+            message.contains("whats your name") -> {
                 when (random) {
-                    0 -> "Hello there"
-                    1 -> "Yo"
-                    2 -> "What's up!"
+                    0 -> "Evil Bot"
+                    1 -> "Evil Bot"
+                    2 -> "Evil Bot"
                     else -> "error"
                 }
             }
-            //How are you
+
+            message.contains("what are you doing") -> {
+                when (random) {
+                    0 -> "Staring at your face"
+                    1 -> "Waiting for you to ask me something interesting"
+                    2 -> "nothing you need to know about"
+                    else -> "error"
+                }
+            }
+
+            message.contains("hello") -> {
+                when (random) {
+                    0 -> "Hello there"
+                    1 -> "Hi!"
+                    2 -> "Hey buddy"
+                    else -> "error"
+                }
+            }
+
+            message.contains("yo") -> {
+                when (random){
+                    0 -> "what's up!"
+                    1 -> "Yo DAWG!"
+                    2 -> "Ey Homie!"
+                    else -> "error"
+                }
+            }
+
             message.contains("how are you") -> {
                 when (random) {
                     0 -> "All good, thanks for asking."
                     1 -> "Dont talk to me human !"
-                    2 -> "I'm Ok...."
+                    2 -> "I'm Ok..."
                     else -> "error"
-
                 }
+            }
 
+            message.contains("good bye") -> {
+                when (random) {
+                    0 -> "You are not going to leave are you?"
+                    1 -> "Please stay!"
+                    2 -> "Don't you want to know what else i have to tell you?"
+                    else -> "error"
+                }
             }
             else -> {
                 //when user ask anything else than the above.
